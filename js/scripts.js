4a1e000c4d66702a6ec55964b871123d52214d69
Brishna Bakshev <b.bakshev@gmail.com>
2023-01-27 10:00:06 -0800

    added a new property for price and it passed the test

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,29 @@
 function Pizza(size) {
   this.size = size;
   this.toppings = [];
+  this.price = 0
 }
 
 Pizza.prototype.addToppings = function (topping) {
   this.toppings.push(topping);
+}
+
+Pizza.prototype.pricePerSize = function() {
+  const price = 0
+
+  if(this.size === "small") {
+    this.price = 12;
+  } else if (this.size === "medium") {
+    this.price = 14;
+  } else if (this.size === "large") {
+    this.price = 16;
+  } else if (this.size === "extra large") {
+    this.price = 18;
+  }
+
+  this.toppings.forEach(function() {
+    this.price += 2;
+  })
+
+  return this.price;
 }
